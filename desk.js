136dc01e7e2449e74e77b73ef5b5ed3ae1d50b3d
shubhajit12 <118266831+shubhajit12@users.noreply.github.com>
2023-07-20 21:04:03 +0530

    Add files via upload

---
+++ desk.js
@@ -0,0 +1,42 @@
+
+img="";
+status1="";
+object=[];
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectdetector=ml5.objectDetector("cocossd",modelloaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+function preload(){
+    img=loadImage("https://i.postimg.cc/prTmKycQ/download-1.jpg");
+}
+function draw(){
+    image(img,0,0,640,420);
+    if(status1!=""){
+        for(i=0; i<object.length; i++){
+            fill("red");
+            percent=floor(object[i].confidence*100);
+            text(object[i].label+" "+percent+"%",object[i].x,object[i].y);
+            noFill();
+            stroke("red");
+            rect(object[i].x,object[i].y,object[i].width,object[i].height);
+            document.getElementById("status").innerHTML="Status: Object Detected";
+            document.getElementById("number_of_objects").innerHTML="Number Of Objects Detected Are "+object.length;
+        }
+    }
+}
+function modelloaded(){
+    console.log("Model Loaded");
+    objectdetector.detect(img,gotresults);
+    status1="tree";
+}
+function gotresults(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        object=results;
+    }
+}
---
+++ tv.js
@@ -0,0 +1,41 @@
+img="";
+status1="";
+object=[];
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectdetector=ml5.objectDetector("cocossd",modelloaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+function preload(){
+    img=loadImage("images.jpg");
+    if(status1!=""){
+        for(i=0; i<object.length; i++){
+            fill("red");
+            percent=floor(object[i].confidence*100);
+            text(object[i].label+" "+percent+"%",object[i].x,object[i].y);
+            noFill();
+            stroke("red");
+            rect(object[i].x,object[i].y,object[i].width,object[i].height);
+            document.getElementById("status").innerHTML="Status: Object Detected";
+            document.getElementById("number_of_objects").innerHTML="Number Of Objects Detected Are "+object.length;
+        }
+    }
+}
+function draw(){
+    image(img,0,0,640,420);
+}
+function modelloaded(){
+    console.log("Model Loaded");
+    objectdetector.detect(img,gotresults);
+    status1="tree";
+}
+function gotresults(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        object=results;
+    }
+}
